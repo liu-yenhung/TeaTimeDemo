22ab3f2d155c789b87550c3f80f3b61a069e5923
liu <l.yenhung@gmail.com>
2025-06-27 22:02:06 +0800

    first commit

---
+++ TeaTimeDemo/Controllers/CategoryController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TeaTimeDemo.Data;
+using TeaTimeDemo.Models;
+
+namespace TeaTimeDemo.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "名稱與順序不能一樣");
+            }
+
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+
+            }
+
+            return View();
+        }
+            
+    }
+}
---
+++ TeaTimeDemo/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeaTimeDemo.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("類別名稱")]
+        public string Name { get; set; }
+        [DisplayName("顯示順序")]
+        [Range(1,100,ErrorMessage ="輸入範圍介於1~100")]
+        public int DisplayOrder { get; set; }
+
+    }
+}
