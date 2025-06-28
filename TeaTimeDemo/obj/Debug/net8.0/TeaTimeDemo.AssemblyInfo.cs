b82950b21c3d6b7fd810e8e0251a3720891f48d1
liu <l.yenhung@gmail.com>
2025-06-28 11:02:45 +0800

    first commit

--- TeaTimeDemo/obj/Debug/net8.0/TeaTimeDemo.AssemblyInfo.cs
+++ TeaTimeDemo/obj/Debug/net8.0/TeaTimeDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeaTimeDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31cd386854113051aaed4cca380b93c66e69060e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22ab3f2d155c789b87550c3f80f3b61a069e5923")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeaTimeDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeaTimeDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
