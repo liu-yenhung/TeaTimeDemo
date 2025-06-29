e9a895f80cd28aebcee53c2d2cc4640130eb302b
liu <l.yenhung@gmail.com>
2025-06-29 17:37:47 +0800

    first commit

--- TeaTimeDemo/Controllers/CategoryController.cs
+++ TeaTimeDemo/Controllers/CategoryController.cs
@@ -40,6 +40,10 @@ namespace TeaTimeDemo.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+
+                TempData["Success"] = "類別新增成功";
+
+
                 return RedirectToAction("Index");
 
 
@@ -47,6 +51,78 @@ namespace TeaTimeDemo.Controllers
 
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+
+                TempData["Success"] = "類別修改成功";
+
+
+                return RedirectToAction("index");
+
+            }
+
+            return View();
+
+        }
+        
+        
+        public IActionResult Delete(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+ 
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+
+            TempData["Success"] = "類別刪除成功";
+
+
+            return RedirectToAction("index");
+        }
+
+
             
     }
 }
--- TeaTimeDemo/obj/Debug/net8.0/TeaTimeDemo.AssemblyInfo.cs
+++ TeaTimeDemo/obj/Debug/net8.0/TeaTimeDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeaTimeDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8857ed1a7b73f1e9793805769f24e22f5619ef1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d99bad9bcb328abb9a76d323dfcf8c2e765da94")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeaTimeDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeaTimeDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
